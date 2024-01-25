f77b06da4444060afe5f397128dc95b60e927783
hellgamer <19thanks96@gmail.com>
2024-01-25 14:18:31 +0200

    add new volunteers, change favicon

--- src/imgs/volunteers/Volunteers.img.js
+++ src/imgs/volunteers/Volunteers.img.js
@@ -1,21 +1,30 @@
-import a  from './1.jpg';
-import b  from './2.jpg';
-import c  from './3.jpg';
-import d  from './4.jpg';
-import e  from './5.jpg';
-import f  from './6.jpg';
-import g  from './7.jpg';
-import h  from './8.jpg';
-import i  from './9.jpg';
-import j  from './10.jpg';
-import k  from './11.jpg';
-import l  from './12.jpg';
-import m  from './13.jpg';
-import n  from './14.jpg';
-import o  from './15.jpg';
-import p  from './16.jpg';
+import a from './1.jpg';
+import b from './2.jpg';
+import c from './3.jpg';
+import d from './4.jpg';
+import e from './5.jpg';
+import f from './6.jpg';
+import g from './7.jpg';
+import h from './8.jpg';
+import i from './9.jpg';
+import j from './10.jpg';
+import k from './11.jpg';
+import l from './12.jpg';
+import m from './13.jpg';
+import n from './14.jpg';
+import o from './15.jpg';
+import p from './16.jpg';
+import q from './17.jpg';
+import r from './18.jpg';
+import s from './19.jpg';
+import t from './20.jpg';
+import u from './21.jpg';
+import v from './22.jpg';
+import w from './23.jpg';
+import x from './24.jpg';
+import y from './25.jpg';
 
-const images = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p};
+const images = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y };
 
 
 export default images
